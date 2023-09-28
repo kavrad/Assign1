21a4fac39df325dd4595395363d94f45c7aa0ecd
kavrad <kavyaht39@gmail.com>
2023-09-28 19:43:35 +0530

    Create call route

--- routes/call.js
+++ routes/call.js
@@ -1,12 +1,15 @@
 //require express
+
 const express=require("express");
 
 const router=express.Router();
 
 //require call controller
+
 const {callController}=require("../controllers/callController");
 
 //post request to make a call
+
 router.post("/call",callController);
 
 module.exports=router;
